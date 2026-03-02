e051a0db8817535fff911a203aa5cada2881250c
sugiartofahmi <sugiartofahmi@gmail.com>
2026-03-02 15:48:22 +0700

    feat: route detail category

--- presentation/http/category/controllers/category_controller.go
+++ presentation/http/category/controllers/category_controller.go
@@ -8,6 +8,8 @@ import (
 	categoryInterfaces "event-backend/app/category/interfaces"
 	"event-backend/infrastructure/utils"
 	categoryDtos "event-backend/presentation/http/category/dtos"
+
+	uuidValidator "event-backend/infrastructure/validators"
 )
 
 type CategoryController struct {
@@ -22,6 +24,7 @@ func NewCategoryController(router *gin.Engine, categoryService categoryInterface
 	}
 
 	categoryRoute.GET("", controller.Pagination())
+	categoryRoute.GET("/:id", controller.Detail())
 }
 
 func (controller *CategoryController) Pagination() gin.HandlerFunc {
@@ -32,6 +35,17 @@ func (controller *CategoryController) Pagination() gin.HandlerFunc {
 		meta := utils.PaginationMetaBuilder(dto.Page, dto.PerPage, int(result.Count))
 		items := categoryDtos.CategoryResponseDtoFromEntities(result.Data)
 		response := utils.SuccessResponsePagination(http.StatusOK, items, *meta)
+
+		httpContext.JSON(http.StatusOK, response)
+	}
+}
+
+func (controller *CategoryController) Detail() gin.HandlerFunc {
+	return func(httpContext *gin.Context) {
+		ctx := httpContext.Request.Context()
+		id := uuidValidator.ValidateUUID(httpContext.Param("id"))
+		result := controller.categoryService.Detail(ctx, id)
+		response := utils.SuccessResponse(http.StatusOK, categoryDtos.CategoryDetailResponseDtoFromEntity(*result))
 
 		httpContext.JSON(http.StatusOK, response)
 	}
