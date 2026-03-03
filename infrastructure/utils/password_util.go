11000679e82228f7d28cb6815b547f9fa7747c3c
sugiartofahmi <sugiartofahmi@gmail.com>
2026-03-03 09:44:06 +0700

    chore: create utils jwt and password

---
+++ infrastructure/utils/password_util.go
@@ -0,0 +1,23 @@
+package utils
+
+import (
+	"event-backend/infrastructure/exceptions"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+func HashPassword(password string) string {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		panic(*exceptions.ServerErrorException(err))
+	}
+	return string(hash)
+}
+
+func ComparePassword(hash, password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	if err != nil {
+		return false
+	}
+	return true
+}
