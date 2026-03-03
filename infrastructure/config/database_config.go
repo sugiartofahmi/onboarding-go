2f81504c69e40e7698c1b3ba89c39ca456a0f4d0
sugiartofahmi <sugiartofahmi@gmail.com>
2026-03-03 09:43:46 +0700

    fix: user seeder and circurlar dependency utils

--- infrastructure/config/database_config.go
+++ infrastructure/config/database_config.go
@@ -1,6 +1,9 @@
 package config
 
-import "event-backend/infrastructure/utils"
+import (
+	"os"
+	"strconv"
+)
 
 var (
 	DBHost                      = GetRequired("DB_HOST")
@@ -9,8 +12,24 @@ var (
 	DBUser                      = GetRequired("DB_USER")
 	DBPassword                  = GetRequired("DB_PASSWORD")
 	DBTimezone                  = Get("DB_TIMEZONE", "Asia/Jakarta")
-	DBMaxIdleConns              = utils.StringToInt(Get("DB_MAX_IDLE_CONNS", "5"))
-	DBMaxOpenConns              = utils.StringToInt(Get("DB_MAX_OPEN_CONNS", "10"))
-	DBMaxIdleConnsInMinutes     = utils.StringToInt(Get("DB_MAX_IDLE_CONNS_IN_MINUTES", "10"))
-	DBMaxLifetimeConnsInMinutes = utils.StringToInt(Get("DB_MAX_LIFETIME_CONNS_IN_MINUTES", "60"))
+	DBMaxIdleConns              = stringToInt(Get("DB_MAX_IDLE_CONNS", "5"))
+	DBMaxOpenConns              = stringToInt(Get("DB_MAX_OPEN_CONNS", "10"))
+	DBMaxIdleConnsInMinutes     = stringToInt(Get("DB_MAX_IDLE_CONNS_IN_MINUTES", "10"))
+	DBMaxLifetimeConnsInMinutes = stringToInt(Get("DB_MAX_LIFETIME_CONNS_IN_MINUTES", "60"))
 )
+
+func stringToInt(value string) int {
+	intValue, err := strconv.Atoi(value)
+	if err != nil {
+		return 0
+	}
+	return intValue
+}
+
+func GetEnv(key, defaultValue string) string {
+	value := os.Getenv(key)
+	if value != "" {
+		return value
+	}
+	return defaultValue
+}
