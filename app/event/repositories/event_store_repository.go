ab75c8b07f11a9c4bb1bdba355b4dac32af4fb28
sugiartofahmi <sugiartofahmi@gmail.com>
2026-03-04 15:36:54 +0700

    feat: event store repo create DI with transaction

--- app/event/repositories/event_store_repository.go
+++ app/event/repositories/event_store_repository.go
@@ -6,17 +6,27 @@ import (
 
 	"gorm.io/gorm"
 
+	"event-backend/app/event/interfaces"
 	"event-backend/entities"
 	"event-backend/infrastructure/exceptions"
 )
 
 type EventStoreRepository struct {
+	db         *gorm.DB
 	eventModel *gorm.DB
 }
 
 func NewEventStoreRepository(db *gorm.DB) *EventStoreRepository {
 	return &EventStoreRepository{
+		db:         db,
 		eventModel: db.Model(&entities.EventEntity{}),
+	}
+}
+
+func (repo *EventStoreRepository) WithTransaction(tx *gorm.DB) interfaces.EventStoreRepositoryInterface {
+	return &EventStoreRepository{
+		db:         tx,
+		eventModel: tx.Model(&entities.EventEntity{}),
 	}
 }
 
