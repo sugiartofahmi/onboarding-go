c5e59254b42cb78e3360006538701c63e23669fa
sugiartofahmi <sugiartofahmi@gmail.com>
2026-03-09 12:55:36 +0700

    feat: user filter by role id

--- presentation/http/user/dtos/user_query_request_dto.go
+++ presentation/http/user/dtos/user_query_request_dto.go
@@ -2,12 +2,14 @@ package dtos
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 
 	infradtos "event-backend/infrastructure/dtos"
 )
 
 type UserQueryRequestDto struct {
 	infradtos.PaginationQueryRequestDto
+	RoleId *uuid.UUID `form:"role_id"`
 }
 
 func AssignUserQueryRequestDto(c *gin.Context) *UserQueryRequestDto {
